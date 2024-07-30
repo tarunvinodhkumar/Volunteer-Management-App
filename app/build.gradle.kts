cb4f049d59e04df097e9d36eff187d9f664fb68c
tarunvinodhkumar <tarunvinodh@gmail.com>
2024-07-30 02:20:11 -0400

    Reworked UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,9 +47,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation ("com.google.android.gms:play-services-auth:20.1.0")
-    implementation (platform("com.google.firebase:firebase-bom:31.5.0"))
-    implementation ("com.google.firebase:firebase-auth-ktx")
-    implementation ("com.google.android.gms:play-services-auth:20.5.0")
 
 }
