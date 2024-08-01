be1a873bfaf5aaea1d5bd6bc52f6f3fd695a6b86
tarunvinodhkumar <tarunvinodh@gmail.com>
2024-08-01 00:38:30 -0400

    Changed Event List Page-Toggle Working

--- app/src/main/java/com/example/volunteermanagementapp/eventlist.kt
+++ app/src/main/java/com/example/volunteermanagementapp/eventlist.kt
@@ -2,57 +2,68 @@ package com.example.volunteermanagementapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButtonToggleGroup
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.QuerySnapshot
-import com.google.firebase.ktx.Firebase
 
-class eventlist: AppCompatActivity() {
+class eventlist : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var eventArrayList: ArrayList<Event>
-    private lateinit var EventAdapter: EventAdapter
+    private lateinit var eventAdapter: EventAdapter
     private lateinit var db: FirebaseFirestore
     private lateinit var auth: FirebaseAuth
 
-
-
-
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.events_list)
 
-
+        auth = FirebaseAuth.getInstance()
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
 
         eventArrayList = arrayListOf()
+        eventAdapter = EventAdapter(eventArrayList)
+        recyclerView.adapter = eventAdapter
 
-        EventAdapter = EventAdapter(eventArrayList)
+        // Set up the toggle button group
+        val toggleGroup = findViewById<MaterialButtonToggleGroup>(R.id.materialButtonToggleGroup)
+        toggleGroup.addOnButtonCheckedListener { _, checkedId, isChecked ->
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.events_toggle -> {
+                        // Navigate to EventsActivity
+                        val intent = Intent(this, eventlist::class.java)
+                        startActivity(intent)
+                    }
+                    R.id.volunteers_toggle -> {
+                        // Navigate to VolunteersActivity
+                        val intent = Intent(this, volunteerList::class.java)
+                        startActivity(intent)
+                    }
+                }
+            }
+        }
 
-        recyclerView.adapter = EventAdapter
-
-        EventChangeListener()
-
-
-
+        eventChangeListener()
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_actionbar, menu)
         return true
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_sign_out -> {
@@ -66,57 +77,23 @@ class eventlist: AppCompatActivity() {
         }
     }
 
-//    // Find the buttons by their IDs
-//    val eventsToggle: Button = findViewById(R.id.events_toggle)
-//    val volunteersToggle: Button = findViewById(R.id.volunteers_toggle)
-
-//    // Set up the toggle buttons
-//    events_toggle.setOnClickListener {
-//        // Navigate to EventsActivity
-//        val intent = Intent(this, EventsActivity::class.java)
-//        startActivity(intent)
-//    }
-//
-//    volunteers_toggle.setOnClickListener {
-//        // Navigate to VolunteersActivity
-//        val intent = Intent(this, VolunteersActivity::class.java)
-//        startActivity(intent)
-//    }
-//
-    private fun EventChangeListener() {
-
+    private fun eventChangeListener() {
         db = FirebaseFirestore.getInstance()
-        db.collection("event_details").
-                addSnapshotListener(object : EventListener<QuerySnapshot>{
-                    override fun onEvent(
-                        value: QuerySnapshot?,
-                        error: FirebaseFirestoreException?
-                    ) {
+        db.collection("event_details").addSnapshotListener(object : EventListener<QuerySnapshot> {
+            override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
+                if (error != null) {
+                    Log.e("Firestore Error", error.message.toString())
+                    return
+                }
 
-                        if (error != null){
-
-                            Log.e("Firestore Error",error.message.toString())
-                            return
-                        }
-
-                        for (dc : DocumentChange in value?.documentChanges!!){
-
-
-                            if (dc.type == DocumentChange.Type.ADDED){
-
-                                eventArrayList.add(dc.document.toObject(Event::class.java))
-                            }
-
-
-                        }
-
-                        EventAdapter.notifyDataSetChanged()
-
+                for (dc: DocumentChange in value?.documentChanges!!) {
+                    if (dc.type == DocumentChange.Type.ADDED) {
+                        eventArrayList.add(dc.document.toObject(Event::class.java))
                     }
+                }
 
-
-                })
-
-
+                eventAdapter.notifyDataSetChanged()
+            }
+        })
     }
 }
--- app/src/main/java/com/example/volunteermanagementapp/volunteerList.kt
+++ app/src/main/java/com/example/volunteermanagementapp/volunteerList.kt
@@ -2,56 +2,68 @@ package com.example.volunteermanagementapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButtonToggleGroup
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.QuerySnapshot
-import com.google.firebase.ktx.Firebase
 
-class volunteerList: AppCompatActivity() {
+class volunteerList : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var volunteerArrayList: ArrayList<Volunteer>
-    private lateinit var VolunteerAdapter: VolunteerAdapter
+    private lateinit var volunteerAdapter: VolunteerAdapter
     private lateinit var db: FirebaseFirestore
     private lateinit var auth: FirebaseAuth
 
-
-
-
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.volunteers_list)
 
-
+        auth = FirebaseAuth.getInstance()
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
 
         volunteerArrayList = arrayListOf()
+        volunteerAdapter = VolunteerAdapter(volunteerArrayList)
+        recyclerView.adapter = volunteerAdapter
 
-        VolunteerAdapter = VolunteerAdapter(volunteerArrayList)
+        // Set up the toggle button group
+        val toggleGroup = findViewById<MaterialButtonToggleGroup>(R.id.materialButtonToggleGroup)
+        toggleGroup.addOnButtonCheckedListener { _, checkedId, isChecked ->
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.events_toggle -> {
+                        // Navigate to EventsActivity
+                        val intent = Intent(this, eventlist::class.java)
+                        startActivity(intent)
+                    }
+                    R.id.volunteers_toggle -> {
+                        // Navigate to VolunteersActivity
+                        val intent = Intent(this, volunteerList::class.java)
+                        startActivity(intent)
+                    }
+                }
+            }
+        }
 
-        recyclerView.adapter = VolunteerAdapter
-
-        EventChangeListener()
-
-
-
+        eventChangeListener()
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_actionbar, menu)
         return true
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_sign_out -> {
@@ -65,40 +77,23 @@ class volunteerList: AppCompatActivity() {
         }
     }
 
-    private fun EventChangeListener() {
-
+    private fun eventChangeListener() {
         db = FirebaseFirestore.getInstance()
-        db.collection("volunteer_details").
-        addSnapshotListener(object : EventListener<QuerySnapshot>{
-            override fun onEvent(
-                value: QuerySnapshot?,
-                error: FirebaseFirestoreException?
-            ) {
-
-                if (error != null){
-
-                    Log.e("Firestore Error",error.message.toString())
+        db.collection("volunteer_details").addSnapshotListener(object : EventListener<QuerySnapshot> {
+            override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
+                if (error != null) {
+                    Log.e("Firestore Error", error.message.toString())
                     return
                 }
 
-                for (dc : DocumentChange in value?.documentChanges!!){
-
-
-                    if (dc.type == DocumentChange.Type.ADDED){
-
+                for (dc: DocumentChange in value?.documentChanges!!) {
+                    if (dc.type == DocumentChange.Type.ADDED) {
                         volunteerArrayList.add(dc.document.toObject(Volunteer::class.java))
                     }
-
-
                 }
 
-                VolunteerAdapter.notifyDataSetChanged()
-
+                volunteerAdapter.notifyDataSetChanged()
             }
-
-
         })
-
-
     }
 }
